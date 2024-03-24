563bea9b1e753111f23e3fd0c3118b1251737be6
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2024-03-24 19:54:12 +0530

    Update time.js

--- solidity/test/time.js
+++ solidity/test/time.js
@@ -1,3 +1,18 @@
+/* Ganache provides a way to move forward in time through two helper functions:
+
+evm_increaseTime: increases the time for the next block.
+evm_mine: mines a new block.
+You don't even need a Tardis or a DeLorean for this kind of time travel.
+
+Let me explain how these functions work:
+
+Every time a new block gets mined, the miner adds a timestamp to it. Let's say the transactions that created our zombies got mined in block 5.
+
+Next, we call evm_increaseTime but, since the blockchain is immutable, there is no way of modifying an existing block. So, when the contract checks the time, it will not be increased.
+
+If we run evm_mine, block number 6 gets mined (and timestamped) which means that, when we put the zombies to fight, the smart contract will "see" that a day has passed.
+*/
+
 async function increase(duration) {
 
     //first, let's increase time
